cd2ff5abd02826cbaff260aef114751ea1bd0839
Florian Springfeldt <florianspringfeldt@icloud.com>
2026-01-17 15:02:26 +0100

    Initial Commit

---
+++ CalendarApp_2/CalendarApp_2App.swift
@@ -0,0 +1,32 @@
+//
+//  CalendarApp_2App.swift
+//  CalendarApp_2
+//
+//  Created by Florian Springfeldt on 17.01.26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct CalendarApp_2App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
